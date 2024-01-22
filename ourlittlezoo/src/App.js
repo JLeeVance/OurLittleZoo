54fe4e57e96521cd6d2049fbfbd30a0c2bcfe4ea
J Vance <justinleevance94@gmail.com>
2024-01-22 12:24:21 -0500

    data

---
+++ ourlittlezoo/src/App.js
@@ -0,0 +1,22 @@
+import { useState , useEffect } from "react"
+import './App.css';
+
+function App() {
+
+  const [ animals , setAnimals ] = useState([]);
+
+  useEffect(() => {
+  fetch("http://localhost:3001/animals")
+  .then(r => r.json())
+  .then(setAnimals)} , [] )
+
+  const animalsToTest = animals.map((animalObj) => <img src={animalObj.animalUrl}  />)
+
+  return (
+    <div>
+      {animalsToTest}
+    </div>
+  );
+}
+
+export default App;
